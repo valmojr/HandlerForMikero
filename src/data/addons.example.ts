d4f9b7105b4d7868fae1dda2c04afbaaf997226b
valmojr <val_mo_jr@hotmail.com>
2023-11-10 16:23:22 -0300

    created example addon data file

---
+++ src/data/addons.example.ts
@@ -0,0 +1,16 @@
+import { Data } from "../types/entities";
+
+const data: Data = [
+    {
+        name: "test",
+        binarized: true,
+        obfuscated: false
+    },
+    {
+        name: "test2",
+        binarized: true,
+        obfuscated: true
+    }
+]
+
+export default data;
