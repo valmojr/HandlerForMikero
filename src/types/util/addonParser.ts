b95ad00e8be2385a5a99736ed9034770be8d456b
valmojr <val_mo_jr@hotmail.com>
2023-11-11 20:22:43 -0300

    deleted addon parser function

--- src/types/util/addonParser.ts
+++
@@ -1,8 +0,0 @@
-import { Addon } from "../entities";
-
-export default function AddonParser(addon: Addon) {
-    (addon.binarized === undefined) ? addon.binarized = true: null;
-    (addon.obfuscated === undefined) ? addon.obfuscated = true : null;
-    
-    return addon;
-}
