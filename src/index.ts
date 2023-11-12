dbc41ebd88d23004536093c8c824aecf8c304e57
valmojr <val_mo_jr@hotmail.com>
2023-11-12 01:06:27 -0300

    index got the initial structure of the pboProject packing for binarized or obfuscated addons

--- src/index.ts
+++ src/index.ts
@@ -21,5 +21,20 @@ const logger = new Logger();
 
     const changedAddons = ChangedAddonHandler(changedAddonsPathes, data);
 
-    logger.log(changedAddons)
+    changedAddons.forEach(async (addon) => {
+        logger.log(`Packing ${addon.name}`);
+        if (addon.obfuscated) {
+            logger.log(`${addon.name} is obfuscated`);
+            await run('echo "Obfuscated"');
+            return;
+        } else if (addon.binarized) {
+            logger.log(`${addon.name} is binarized`);
+            await run('echo "Binarized"');
+            return;
+        } else {
+            logger.log(`${addon.name} is not obfuscated or binarized`);
+            await run('echo "Not obfuscated or binarized"');
+        }
+        await run(`echo ${addon.name}`);
+    });
 })();
