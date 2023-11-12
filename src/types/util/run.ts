a0616ecbd498822771d0e0cc681eb0cd39b39ed7
valmojr <val_mo_jr@hotmail.com>
2023-11-11 21:07:42 -0300

    added run function to run bash commands

---
+++ src/types/util/run.ts
@@ -0,0 +1,16 @@
+import { exec } from "child_process";
+import { promisify } from "util";
+import Logger from "./logger";
+
+const logger = new Logger();
+const execPromise = promisify(exec);
+
+const Run = async (command: string) => {
+    try {
+        const { stdout } = await execPromise(command);
+        return stdout.trim(); // Trim any extra whitespace
+    } catch (error) {
+        logger.error(error);
+}};
+
+export default Run;
