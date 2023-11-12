a7ad9aa719450051ad0c991395c0e80cc4de82e2
valmojr <val_mo_jr@hotmail.com>
2023-11-12 00:37:05 -0300

    index printing changed addons object array

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,25 @@
-import { CheckAndAddToChangedAddons, GetRootDir } from "./types/util/FileHandler";
-import Logger from "./types/util/logger";
-import run from "./types/util/run";
+import data from "./data/addons";
+import { CheckAndAddToChangedAddons, GetRootDir, ChangedAddonHandler } from "./functions/util/FileHandler";
+import Logger from "./functions/util/logger";
+import run from "./functions/util/run";
 
 const logger = new Logger();
 
 (async () => {
     const commitOnPull = await run("git rev-parse HEAD");
-    const commitOnOrigin = (await run("git ls-remote origin -h refs/heads/main"))?.replace("refs/heads/main", "").trim();
+    const commitOnOrigin = (await run("git ls-remote origin -h refs/heads/main"))
+        ?.replace("refs/heads/main", "")
+        .trim();
 
     const changedFiles = (await run(`git diff --name-only ${commitOnPull} ${commitOnOrigin}`))?.split("\n");
 
-    const changedAddons: string[] = [];
+    const changedAddonsPathes: string[] = [];
 
     changedFiles?.forEach(async (file) => {
-        CheckAndAddToChangedAddons(changedAddons, GetRootDir(file));
+        CheckAndAddToChangedAddons(changedAddonsPathes, GetRootDir(file));
     })
 
+    const changedAddons = ChangedAddonHandler(changedAddonsPathes, data);
 
-    logger.log(changedAddons);
+    logger.log(changedAddons)
 })();
