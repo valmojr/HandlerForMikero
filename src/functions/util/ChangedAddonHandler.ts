11bf2a70eee49af461bbeaf8e510daeb94bd540f
valmojr <val_mo_jr@hotmail.com>
2023-11-12 00:36:52 -0300

    moved changed addons function to the concat file

--- src/functions/util/ChangedAddonHandler.ts
+++
@@ -1,6 +0,0 @@
-import { Data } from "../../types/entities";
-
-// return the addons from data that are on changedAddons
-export default function ChangedAddonHandler(changedAddons: string[], data: Data) {
-    return data.filter((addon) => changedAddons.includes(addon.name));
-}
--- src/functions/util/FileHandler.ts
+++ src/functions/util/FileHandler.ts
@@ -1,3 +1,5 @@
+import { Data } from "../../types/entities";
+
 // separate only the root directory from the filepath
 export function GetRootDir(file: string) {
     return file.split("/")[0];
@@ -10,4 +12,9 @@ export function CheckAndAddToChangedAddons(changedAddons: string[],addonName: st
     }
 
     return changedAddons;
+}
+
+// return the addons from data that are on changedAddons
+export function ChangedAddonHandler(changedAddons: string[], data: Data) {
+    return data.filter((addon) => changedAddons.includes(addon.name));
 }
