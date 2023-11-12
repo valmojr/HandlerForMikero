c43d46e9a4eb938536e3718cb7df905a64be96b1
valmojr <val_mo_jr@hotmail.com>
2023-11-11 23:32:10 -0300

    index printing changed files

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello World!')
+import Logger from "./types/util/logger";
+import run from "./types/util/run";
+
+const logger = new Logger();
+
+(async () => {
+    const commitOnPull = await run("git rev-parse HEAD");
+    const commitOnOrigin = (await run("git ls-remote origin -h refs/heads/main"))?.replace("refs/heads/main", "").trim();
+
+    const changedFiles = (await run(`git diff --name-only ${commitOnPull} ${commitOnOrigin}`))?.split("\n");
+
+    changedFiles?.forEach(async (file) => {
+        logger.log(`${file}`);
+    })
+})();
