8ad0b6c0914f9f6b3014959966834da2f2be49e8
valmojr <val_mo_jr@hotmail.com>
2023-11-11 21:07:24 -0300

    changed logger type param to any to log everything

--- src/types/util/logger.ts
+++ src/types/util/logger.ts
@@ -1,17 +1,17 @@
 export default class Logger {
-    log = (message: string) => {
+    log = (message: any) => {
         message = `\x1b[34m[LOG] ${message}\x1b[0m`;
         console.log(message);
         
         return message;
     }
-    warn = (message: string) => {
+    warn = (message: any) => {
         message = `\x1b[33m[WARN] ${message}\x1b[0m`;
         console.log(message);
         
         return message;
     }
-    error = (message: string) => {
+    error = (message: any) => {
         message = `\x1b[41m[ERROR] ${message}\x1b[0m`;
         console.log(message);
         
