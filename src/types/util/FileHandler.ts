fc3a30525100c8db28781ee09a3d7164d7bb655b
valmojr <val_mo_jr@hotmail.com>
2023-11-11 23:41:13 -0300

    added checkAndAddToChangedAddons function

--- src/types/util/FileHandler.ts
+++ src/types/util/FileHandler.ts
@@ -2,3 +2,10 @@
 export function GetRootDir(file: string) {
     return file.split("/")[0];
 }
+
+// Check if the addon is already in the changed addons array, if not, add it
+export function CheckAndAddToChangedAddons(changedAddons: string[],addonName: string) {
+    if(!changedAddons.includes(addonName)) {
+        changedAddons.push(addonName);
+    }
+}
