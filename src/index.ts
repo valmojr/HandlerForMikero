da5ff58c140fd9a8388550d6db3f605a24042f4e
Valmo Trindade <70849974+valmojr@users.noreply.github.com>
2023-11-17 17:34:26 +0000

    changed main boostrap function to a separted file

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,6 @@
-import AddonPacker from "./functions/AddonPacker";
-import DataImporter from "./functions/DataImporter";
-import { CheckAndAddToChangedAddons, GetRootDir, ChangedAddonHandler } from "./functions/util/FileHandler";
-import run from "./functions/util/run";
+import Logger from "./functions/util/logger";
+import Bootstrap from "./main";
 
-(async () => {
-    const rootDir = process.argv[2];
+const logger = new Logger();
 
-    const builderJson = process.argv[3];
-
-    let data = null;
-
-    if (builderJson) data = await DataImporter(builderJson);
-
-    await run (`cd ${rootDir}`)
-
-    if (!rootDir) {
-        console.error("Root dir is not provided");
-        process.exit(1);
-    }
-
-    const commitOnPull = await run("git rev-parse HEAD");
-    const commitOnOrigin = (await run("git ls-remote origin -h refs/heads/main"))
-        ?.replace("refs/heads/main", "")
-        .trim();
-
-    const changedFiles = (await run(`git diff --name-only ${commitOnPull} ${commitOnOrigin}`))?.split("\n");
-
-    const changedAddonsPathes: string[] = [];
-
-    changedFiles?.forEach(async (file) => {
-        CheckAndAddToChangedAddons(changedAddonsPathes, GetRootDir(file));
-    })
-
-    const changedAddons = ChangedAddonHandler(changedAddonsPathes, data);
-
-    await AddonPacker(changedAddons);
-})();
+Bootstrap(logger);
---
+++ src/main.ts
@@ -0,0 +1,40 @@
+import AddonPacker from "./functions/AddonPacker";
+import DataImporter from "./functions/DataImporter";
+import { CheckAndAddToChangedAddons, GetRootDir, ChangedAddonHandler } from "./functions/util/FileHandler";
+import Logger from "./functions/util/logger";
+import run from "./functions/util/run";
+
+export default async function Bootstrap (logger: Logger) {
+    const rootDir = process.argv[2];
+
+    const builderJson = process.argv[3];
+
+    let data = null;
+
+    if (builderJson) data = await DataImporter(builderJson);
+
+    await run(`cd ${rootDir}`)
+
+    if (!rootDir) {
+        logger.error("Root dir is not provided");
+        process.exit(1);
+    }
+
+    const commitOnPull = await run("git rev-parse HEAD");
+
+    const commitOnOrigin = (await run("git ls-remote origin -h refs/heads/main"))
+        ?.replace("refs/heads/main", "")
+        .trim();
+
+    const changedFiles = (await run(`git diff --name-only ${commitOnPull} ${commitOnOrigin}`))?.split("\n");
+
+    const changedAddonsPathes: string[] = [];
+
+    changedFiles?.forEach(async (file) => {
+        CheckAndAddToChangedAddons(changedAddonsPathes, GetRootDir(file));
+    })
+
+    const changedAddons = ChangedAddonHandler(changedAddonsPathes, data);
+
+    await AddonPacker(changedAddons);
+};
